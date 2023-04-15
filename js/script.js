9da0cee4ba8ad74e1d8fe47f08b05bd754d05010
Matias De Bruyn <matutedebruyn@gmail.com>
2023-04-14 21:11:55 -0300

    nuevo

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let previoustitle = document.title;
+window.addEventListener("blur", () => {
+  previoustitle = document.title;
+  document.title = "¡No te vayas! ¡Vuelve!";
+});
+window.addEventListener("focus", () => {
+  document.title = previoustitle;
+});
+
+const el = document.getElementById("poster");
+const height = el.clientHeight;
+const width = el.clientWidth;
+
+el.addEventListener("mousemove", (evt) => {
+  const { layerX, layerY } = evt;
+  const yRotation = ((layerX - width / 2) / width) * 20;
+  const xRotation = ((layerY - height / 2) / height) * 20;
+  const string = `
+  perspective(500px)
+  scale (1.1)
+  rotateX(${xRotation}deg)
+  rotateY(${yRotation}deg)`;
+
+  el.estilos.transform = string;
+});
+
+el.addEventListener("mousemove", (evt) => {
+  el.estlos.transform = `
+perspective(500px)
+scale (1)
+rotateX(0)
+rotateY(0)`;
+});
