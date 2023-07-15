fb49256c1db694e406ab26b8f927db25ae71dbe5
Matias De Bruyn <matutedebruyn@gmail.com>
2023-07-15 14:43:53 -0300

    15/7

--- js/script.js
+++ js/script.js
@@ -9,15 +9,43 @@ window.addEventListener("focus", () => {
 
 function mostrar() {
   document.getElementById(`AboutMe`).style.display = `flex`;
+  document.getElementById(`Principio`).style.display = `none`;
+  document.getElementById(`Education`).style.display = `none`;
+  document.getElementById(`Experience`).style.display = `none`;
+  document.getElementById(`Jobs`).style.display = `none`;
+  document.getElementById(`menu`).style.left = "-100%";
+  document.getElementById(`boton-menu`).style.display = "flex";
 }
 function mostrar2() {
   document.getElementById(`Education`).style.display = `flex`;
+  document.getElementById(`Principio`).style.display = `none`;
+  document.getElementById(`AboutMe`).style.display = `none`;
+  document.getElementById(`Experience`).style.display = `none`;
+  document.getElementById(`Jobs`).style.display = `none`;
+  document.getElementById(`menu`).style.left = "-100%";
+  document.getElementById(`boton-menu`).style.display = "flex";
 }
 function mostrar3() {
   document.getElementById(`Experience`).style.display = `flex`;
+  document.getElementById(`Principio`).style.display = `none`;
+  document.getElementById(`AboutMe`).style.display = `none`;
+  document.getElementById(`Education`).style.display = `none`;
+  document.getElementById(`Jobs`).style.display = `none`;
+  document.getElementById(`menu`).style.left = "-100%";
+  document.getElementById(`boton-menu`).style.display = "flex";
 }
 function mostrar4() {
   document.getElementById(`Jobs`).style.display = `flex`;
+  document.getElementById(`Principio`).style.display = `none`;
+  document.getElementById(`AboutMe`).style.display = `none`;
+  document.getElementById(`Education`).style.display = `none`;
+  document.getElementById(`Experience`).style.display = `none`;
+  document.getElementById(`menu`).style.left = "-100%";
+  document.getElementById(`boton-menu`).style.display = "flex";
+}
+function mostrar5() {
+  document.getElementById(`menu`).style.left = "-100%";
+  document.getElementById(`boton-menu`).style.display = "flex";
 }
 /*
 function ocultar() {
@@ -64,16 +92,21 @@ var menu = document.getElementById("menu");
 var salirmenu = document.getElementById("salirmenu");
 
 botonMenu.addEventListener("click", function () {
-  menu.style.display = "flex";
+  menu.style.left = "0";
   botonMenu.style.display = "none";
 });
 salirmenu.addEventListener("click", function () {
-  menu.style.display = "none";
+  menu.style.left = "-100%";
   botonMenu.style.display = "flex";
 });
 
-/*var apartados = ["AboutMe", "Education", "Experience", "Jobs", "footer"];
+document
+  .getElementById("salirmenu")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+  });
 
+/*
 for (var i = 0; i < apartados.length; i++) {
   var apartado = document.getElementById(apartados[i]);
   apartado.addEventListener("click", function () {
@@ -88,3 +121,21 @@ function pachux() {
 function arquiaires() {
   window.open("https://matiasdbruyn.github.io/ArquiAires/");
 }
+
+var pachux = document.getElementById("pachux");
+pachux.addEventListener("mouseover", function () {
+  document.getElementById("pachuxcont").style.display = "flex";
+});
+
+pachux.addEventListener("mouseout", function () {
+  document.getElementById("pachuxcont").style.display = "none";
+});
+
+var arquiaires = document.getElementById("arquiaires");
+arquiaires.addEventListener("mouseover", function () {
+  document.getElementById("arquicont").style.display = "flex";
+});
+
+arquiaires.addEventListener("mouseout", function () {
+  document.getElementById("arquicont").style.display = "none";
+});
